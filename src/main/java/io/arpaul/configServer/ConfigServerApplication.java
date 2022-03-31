7a831358b92e1124f4907022e5391406230f0311
ArPal <aritrarpal@gmail.com>
2022-03-31 13:46:18 +0530

    Config server setup

--- src/main/java/io/arpaul/configServer/ConfigServerApplication.java
+++ src/main/java/io/arpaul/configServer/ConfigServerApplication.java
@@ -2,12 +2,16 @@ package io.arpaul.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
+//	URL for localhost path
+//	http://localhost:8888/app/profile
 }
